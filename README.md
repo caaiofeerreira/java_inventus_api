bfd39a848a2ca72de2839bb0e7d94cf63d184839
Caio Ferreira <caio.ferreira1914@gmail.com>
2024-12-24 13:52:41 -0300

    iniciando inventus_api - gestao de estoque

---
+++ README.md
@@ -0,0 +1,5 @@
+# INVENTUS - Api de gestão de estoque
+
+<img src="Inventus%20-%20API%20de%20gestão%20de%20estoque.jpg">
+
+---
---
+++ src/test/java/com/inventus/InventusApplicationTests.java
@@ -0,0 +1,13 @@
+package com.inventus;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventusApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
