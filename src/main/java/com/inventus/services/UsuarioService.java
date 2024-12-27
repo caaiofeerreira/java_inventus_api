da5ed30c29b6aad50dc557f4c6bb4f14af2e92fb
Caio Ferreira <caio.ferreira1914@gmail.com>
2024-12-27 11:21:15 -0300

    Refatoracao: movida a logica de login do UsuarioController para o UsuarioService, separando a responsabilidade.

--- src/main/java/com/inventus/services/UsuarioService.java
+++ src/main/java/com/inventus/services/UsuarioService.java
@@ -2,8 +2,12 @@ package com.inventus.services;
 
 import com.inventus.domain.dto.CadastrarUsuarioDto;
 import com.inventus.domain.dto.UsuarioDto;
+import com.inventus.domain.usuario.AutenticacaoUsuario;
+import com.inventus.infra.security.DadosTokenJWT;
 
 public interface UsuarioService {
 
     UsuarioDto cadastrarUsuario(CadastrarUsuarioDto usuarioDto);
+
+    DadosTokenJWT efetuarLogin(AutenticacaoUsuario dados);
 }
--- src/main/java/com/inventus/services/impl/UsuarioServiceImpl.java
+++ src/main/java/com/inventus/services/impl/UsuarioServiceImpl.java
@@ -3,10 +3,18 @@ package com.inventus.services.impl;
 import com.inventus.domain.dto.CadastrarUsuarioDto;
 import com.inventus.domain.dto.UsuarioDto;
 import com.inventus.domain.status.Status;
+import com.inventus.domain.usuario.AutenticacaoUsuario;
 import com.inventus.domain.usuario.Usuario;
+import com.inventus.infra.exception.CredenciaisInvalidasException;
+import com.inventus.infra.security.DadosTokenJWT;
+import com.inventus.infra.security.TokenService;
 import com.inventus.repositories.UsuarioRepository;
 import com.inventus.services.UsuarioService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -21,6 +29,12 @@ public class UsuarioServiceImpl implements UsuarioService {
 
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private AuthenticationManager manager;
+
+    @Autowired
+    private TokenService tokenService;
 
     @Override
     @Transactional
@@ -41,5 +55,18 @@ public class UsuarioServiceImpl implements UsuarioService {
         Usuario novoUsuario = usuarioRepository.save(usuario);
 
         return new UsuarioDto(novoUsuario);
+    }
+
+    @Override
+    public DadosTokenJWT efetuarLogin(AutenticacaoUsuario dados) {
+        try {
+            var autenticacaoToken = new UsernamePasswordAuthenticationToken(dados.email(), dados.senha());
+            var autenticacao = manager.authenticate(autenticacaoToken);
+            var tokenJWT = tokenService.generateToken((Usuario) autenticacao.getPrincipal());
+
+            return new DadosTokenJWT(tokenJWT);
+        } catch (BadCredentialsException | UsernameNotFoundException e) {
+            throw new CredenciaisInvalidasException("Credenciais inválidas fornecidas. Verifique seu email e senha e tente novamente.");
+        }
     }
 }
